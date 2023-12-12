ccaff8762ca199efa2b33616c948e4268b2319ae
DanielFerreira <daniel18mtm@gmail.com>
2023-12-12 14:45:38 -0300

    Reformulando 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,24 @@
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import Login from './components/Login';
+import { createUser } from './services/userAPI';
+import Fill from './components/Fill';
+
 function App() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const login = async (userName: string) => {
+    setLoading(true);
+    await createUser({ name: userName });
+    setLoading(false);
+    navigate('/search');
+  };
+
   return (
-    <p>Trybetunes</p>
+    <Routes>
+      <Route path="/" element={ loading ? <Fill /> : <Login login={ login } /> } />
+    </Routes>
   );
 }
 
---
+++ src/components/Fill.tsx
@@ -0,0 +1,6 @@
+function Fill() {
+  return (
+    <h1>Loading...</h1>
+  );
+}
+export default Fill;
